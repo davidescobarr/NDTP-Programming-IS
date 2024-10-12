1e464b1bdce6373e52157fd67d659c8ccc25dddc
DavidEscobarr <47315634+davidescobarr@users.noreply.github.com>
2024-10-12 11:06:28 +0300

    Create build.sh for automatic build

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+# Define paths to your other bash scripts
+SCRIPT1="./scripts/build_problem_solver.sh"
+SCRIPT2="./scripts/build_kb.sh"
+SCRIPT3="./scripts/build_sc_web.sh"
+
+chmod +x "$SCRIPT1"
+
+chmod +x "$SCRIPT2"
+
+chmod +x "$SCRIPT3"
+
+cd "../"
+source .venv/bin/activate
+cd "nika"
+
+bash "$SCRIPT1"
+bash "$SCRIPT2"
+bash "$SCRIPT3"
+
+echo "All modules are built."
