4559155eb862de699ca37134c653df16f60a12ad
DavidEscobarr <47315634+davidescobarr@users.noreply.github.com>
2024-10-18 08:20:16 +0300

    Delete trash

--- problem-solver/py/modules/messageProcessingModule/SearchProfessionAgent.py
+++ problem-solver/py/modules/messageProcessingModule/SearchProfessionAgent.py
@@ -69,7 +69,6 @@ class SearchProfessionAgent(ScAgentClassic):
             self.logger.info(f"SearchProfessionAgent: finished with an error")
             return ScResult.ERROR
 
-        human_skills = []
         human_skills = self.get_human_skills()
 
         if human_skills is None:
@@ -192,72 +191,3 @@ class SearchProfessionAgent(ScAgentClassic):
             human_skills.append(human_skill_addr.get("_node"))
 
         return human_skills
-
-    def get_ru_idtf(self, entity_addr: ScAddr) -> ScAddr:
-        main_idtf = ScKeynodes.resolve(
-            "nrel_main_idtf", sc_types.NODE_CONST_NOROLE)
-        lang_ru = ScKeynodes.resolve("lang_ru", sc_types.NODE_CONST_CLASS)
-
-        template = ScTemplate()
-        template.triple_with_relation(
-            entity_addr,
-            sc_types.EDGE_D_COMMON_VAR,
-            sc_types.LINK,
-            sc_types.EDGE_ACCESS_VAR_POS_PERM,
-            main_idtf,
-        )
-        search_results = template_search(template)
-        for result in search_results:
-            idtf = result[2]
-            lang_edge = get_edge(
-                lang_ru, idtf, sc_types.EDGE_ACCESS_VAR_POS_PERM)
-            if lang_edge:
-                return idtf
-        return get_element_by_norole_relation(
-            src=entity_addr, nrel_node=main_idtf)
-
-    def get_entity_addr(self, message_addr: ScAddr, rrel_entity: ScAddr):
-        rrel_entity = ScKeynodes.resolve("rrel_entity", sc_types.NODE_ROLE)
-        concept_country = ScKeynodes.resolve(
-            "concept_country", sc_types.NODE_CONST_CLASS)
-        template = ScTemplate()
-        # entity node or link
-
-        template.triple_with_relation(
-            message_addr,
-            sc_types.EDGE_ACCESS_VAR_POS_PERM,
-            sc_types.VAR,
-            sc_types.EDGE_ACCESS_VAR_POS_PERM,
-            rrel_entity,
-        )
-        search_results = template_search(template)
-        if len(search_results) == 0:
-            return ScAddr(0), None
-        entity = search_results[0][2]
-        if len(search_results) == 1:
-            return entity, None
-        # check country position in search_results
-        country_edge = get_edge(
-            concept_country, entity, sc_types.EDGE_ACCESS_VAR_POS_PERM)
-        if country_edge:
-            return search_results[1][2], entity
-        else:
-            return entity, search_results[1][2]
-
-    def clear_previous_answer(self, entity, nrel_temperature, answer_phrase):
-        message_answer_set = ScSet(set_node=answer_phrase)
-        message_answer_set.clear()
-        if not entity.is_valid():
-            return
-
-        template = ScTemplate()
-        template.triple_with_relation(
-            entity,
-            sc_types.EDGE_D_COMMON_VAR,
-            sc_types.LINK,
-            sc_types.EDGE_ACCESS_VAR_POS_PERM,
-            nrel_temperature
-        )
-        search_results = template_search(template)
-        for result in search_results:
-            delete_edges(result[0], result[2], sc_types.EDGE_D_COMMON_VAR)
