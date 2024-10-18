2c88576388251964e7d3a01bddbfcb379e2d64b4
DavidEscobarr <47315634+davidescobarr@users.noreply.github.com>
2024-10-18 08:16:53 +0300

    Adding message about personaly profession

--- problem-solver/py/modules/messageProcessingModule/MessageProcessingModule.py
+++ problem-solver/py/modules/messageProcessingModule/MessageProcessingModule.py
@@ -1,7 +1,8 @@
 from sc_kpm import ScModule
 from .WeatherAgent import WeatherAgent
+from .SearchProfessionAgent import SearchProfessionAgent
 
 
 class MessageProcessingModule(ScModule):
     def __init__(self):
-        super().__init__(WeatherAgent())
+        super().__init__(WeatherAgent(), SearchProfessionAgent())
--- problem-solver/py/modules/messageProcessingModule/SearchProfessionAgent.py
+++ problem-solver/py/modules/messageProcessingModule/SearchProfessionAgent.py
@@ -3,6 +3,8 @@ This code creates some test agent and registers until the user stops the process
 For this we wait for SIGINT.
 """
 import logging
+from re import template
+from typing import List, Optional
 
 from sc_client.models import ScAddr, ScLinkContentType, ScTemplate, ScConstruction
 from sc_client.constants import sc_types
@@ -54,6 +56,8 @@ class SearchProfessionAgent(ScAgentClassic):
                 "concept_profession", sc_types.NODE_CONST_CLASS)
             nrel_profession = ScKeynodes.resolve(
                 "nrel_profession", sc_types.NODE_NOROLE)
+            nrel_skills = ScKeynodes.resolve(
+                "nrel_skills", sc_types.NODE_NOROLE)
             message_type = ScKeynodes.resolve('concept_message_ask_about_personaly_profession', sc_types.NODE_CONST_CLASS)
 
             if not check_edge(sc_types.EDGE_ACCESS_VAR_POS_PERM, message_type, message_addr):
@@ -65,15 +69,129 @@ class SearchProfessionAgent(ScAgentClassic):
             self.logger.info(f"SearchProfessionAgent: finished with an error")
             return ScResult.ERROR
 
-        link = create_link(
-            "Builder", ScLinkContentType.STRING, link_type=sc_types.LINK_CONST)
+        human_skills = []
+        human_skills = self.get_human_skills()
+
+        if human_skills is None:
+            self.logger.info(f"SearchProfessionAgent: human skills is none")
+            return ScResult.ERROR_NOT_FOUND
+
+        if not len(human_skills) > 0:
+            self.logger.info(f"SearchProfessionAgent: human skills not found")
+            return ScResult.OK
+
+        concept_profession = ScKeynodes.resolve(
+            "concept_profession", sc_types.NODE_CONST_CLASS)
+        template = ScTemplate()
+        # entity node or link
+
+        template.triple(
+            concept_profession,
+            sc_types.EDGE_ACCESS_VAR_POS_PERM,
+            sc_types.NODE_VAR >> "_node"
+        )
+
+        result = template_search(template)
+
+        if not len(result) > 0:
+            self.logger.info(
+                f"SearchProfessionAgent: the professions not found")
+            return ScResult.ERROR_NOT_FOUND
+
+        dict_professions = {}
+
+        for addr in result:
+            entity_profession = addr.get("_node")
+            dict_professions[entity_profession] = 0
+
+            template = ScTemplate()
+            # entity node or link
+
+            template.triple_with_relation(
+                entity_profession,
+                sc_types.EDGE_D_COMMON_VAR,
+                sc_types.NODE_VAR >> "_node",
+                sc_types.EDGE_ACCESS_VAR_POS_PERM,
+                nrel_skills
+            )
+
+            result_skills = template_search(template)
+
+            if not len(result_skills) > 0:
+                continue
+
+            for addr_skill in result_skills:
+                skill = addr_skill.get("_node")
+                if not skill:
+                    continue
+                if skill in human_skills:
+                    dict_professions[entity_profession] += 1
+
+        if not len(dict_professions) > 0:
+            self.logger.info(
+                f"SearchProfessionAgent: the professions not found with generals skills")
+            return ScResult.ERROR_NOT_FOUND
+
+        self.logger.info(dict_professions)
+
+        sorted_dict_professions = sorted(dict_professions, key=dict_professions.__getitem__)
+        profession = sorted_dict_professions[-1]
+
+        fireman = ScKeynodes.resolve(
+            "Architector", sc_types.NODE_CONST)
         profession_edge = create_edge(
-            sc_types.EDGE_D_COMMON_CONST, message_addr, link)
-        create_edge(
-            sc_types.EDGE_ACCESS_CONST_POS_PERM, nrel_profession, profession_edge)
-        create_action_answer(action_node, link)
+            sc_types.EDGE_ACCESS_CONST_POS_PERM, message_addr, profession)
+        create_action_answer(action_node, profession_edge)
 
         return ScResult.OK
+
+    def get_human_skills(self):
+        concept_human = ScKeynodes.resolve("concept_human", sc_types.NODE_CONST_CLASS)
+
+        nrel_skills = ScKeynodes.resolve(
+            "nrel_skills", sc_types.NODE_NOROLE)
+
+        template = ScTemplate()
+
+        template.triple(
+            concept_human,
+            sc_types.EDGE_ACCESS_VAR_POS_PERM,
+            sc_types.NODE_VAR >> "_node"
+        )
+
+        result = template_search(template)
+
+        if not len(result) > 0:
+            self.logger.info(
+                f"SearchProfessionAgent: the human not found")
+            return None
+
+        human = result[0].get("_node")
+
+        template = ScTemplate()
+        # entity node or link
+
+        template.triple_with_relation(
+            human,
+            sc_types.EDGE_D_COMMON_VAR,
+            sc_types.NODE_VAR >> "_node",
+            sc_types.EDGE_ACCESS_VAR_POS_PERM,
+            nrel_skills
+        )
+
+        result_human_skills = template_search(template)
+
+        if not len(result_human_skills) > 0:
+            self.logger.info(
+                f"SearchProfessionAgent: the skills human not found")
+            return None
+
+        human_skills = []
+
+        for human_skill_addr in result_human_skills:
+            human_skills.append(human_skill_addr.get("_node"))
+
+        return human_skills
 
     def get_ru_idtf(self, entity_addr: ScAddr) -> ScAddr:
         main_idtf = ScKeynodes.resolve(
@@ -104,6 +222,7 @@ class SearchProfessionAgent(ScAgentClassic):
             "concept_country", sc_types.NODE_CONST_CLASS)
         template = ScTemplate()
         # entity node or link
+
         template.triple_with_relation(
             message_addr,
             sc_types.EDGE_ACCESS_VAR_POS_PERM,
